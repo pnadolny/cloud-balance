3f9df4349d3f5109ae6c44f222d53210b2b6d1da
pnadolny <pnadolny@Sarahs-MacBook-Air.local>
2015-12-23 17:11:14 -0600

    Building app component

--- src/main/webapp/ng2/app/app.component.ts
+++ src/main/webapp/ng2/app/app.component.ts
@@ -1,7 +1,23 @@
 import {Component} from 'angular2/core';
 
+interface Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template:`
+      <h1>{{title}}</h1><h2>{{hero.name}} details!</h2><div>
+      <label>id: </label>{{hero.id}}</div>
+      <div><label>name: </label>{{hero.name}}</div>
+      `
 })
-export class AppComponent { }
+export class AppComponent {
+
+  public hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+}
