d1c3168d73c972f4e74669f145cbca51d3e50967
Paul Nadolny <psnadolny@gmail.com>
2016-07-08 22:42:48 -0500

    Angular 2 development - Use CDN versions

--- src/main/webapp/ng2/systemjs.config.js
+++ src/main/webapp/ng2/systemjs.config.js
@@ -1,21 +1,32 @@
 /**
+ * PLUNKER VERSION (based on systemjs.config.js in angular.io)
  * System configuration for Angular 2 samples
  * Adjust as necessary for your application needs.
  */
 (function(global) {
 
-  // map tells the System loader where to look for things
-  var map = {
-    'app':                        'app', // 'dist',
+  var ngVer = '@2.0.0-rc.4'; // lock in the angular package version; do not let it float to current!
+  var routerVer = '@3.0.0-beta.1'; // lock router version
+  var formsVer = '@0.2.0'; // lock forms version
+  var routerDeprecatedVer = '@2.0.0-rc.2'; // temporarily until we update all the guides
 
-    '@angular':                   'node_modules/@angular',
-    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    'rxjs':                       'node_modules/rxjs'
+  //map tells the System loader where to look for things
+  var  map = {
+    'app':                        'app',
+
+    '@angular':                   'https://npmcdn.com/@angular', // sufficient if we didn't pin the version
+    '@angular/router':            'https://npmcdn.com/@angular/router' + routerVer,
+    '@angular/forms':             'https://npmcdn.com/@angular/forms' + formsVer,
+    '@angular/router-deprecated': 'https://npmcdn.com/@angular/router-deprecated' + routerDeprecatedVer,
+    'angular2-in-memory-web-api': 'https://npmcdn.com/angular2-in-memory-web-api', // get latest
+    'rxjs':                       'https://npmcdn.com/rxjs@5.0.0-beta.6',
+    'ts':                         'https://npmcdn.com/plugin-typescript@4.0.10/lib/plugin.js',
+    'typescript':                 'https://npmcdn.com/typescript@1.9.0-dev.20160409/lib/typescript.js',
   };
 
-  // packages tells the System loader how to load when no filename and/or no extension
+  //packages tells the System loader how to load when no filename and/or no extension
   var packages = {
-    'app':                        { main: 'boot.js',  defaultExtension: 'js' },
+    'app':                        { main: 'boot.ts',  defaultExtension: 'ts' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
   };
@@ -24,35 +35,48 @@
     'common',
     'compiler',
     'core',
-    'forms',
     'http',
     'platform-browser',
     'platform-browser-dynamic',
-    'router',
-    'router-deprecated',
     'upgrade',
   ];
 
-  // Individual files (~300 requests):
-  function packIndex(pkgName) {
-    packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
-  }
-
-  // Bundled (~40 requests):
-  function packUmd(pkgName) {
-    packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
-  }
-
-  // Most environments should use UMD; some (Karma) need the individual index files
-  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+  // Add map entries for each angular package
+  // only because we're pinning the version with `ngVer`.
+  ngPackageNames.forEach(function(pkgName) {
+    map['@angular/'+pkgName] = 'https://npmcdn.com/@angular/' + pkgName + ngVer;
+  });
 
   // Add package entries for angular packages
-  ngPackageNames.forEach(setPackageConfig);
+  ngPackageNames.forEach(function(pkgName) {
+
+    // Bundled (~40 requests):
+    packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+
+    // Individual files (~300 requests):
+    //packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
+  });
 
   // No umd for router yet
   packages['@angular/router'] = { main: 'index.js', defaultExtension: 'js' };
 
+  // Forms not on rc yet
+  packages['@angular/forms'] = { main: 'index.js', defaultExtension: 'js' };
+
+  // Temporarily until we update the guides
+  packages['@angular/router-deprecated'] = { main: '/bundles/router-deprecated' + '.umd.js', defaultExtension: 'js' };
+
   var config = {
+    // DEMO ONLY! REAL CODE SHOULD NOT TRANSPILE IN THE BROWSER
+    transpiler: 'ts',
+    typescriptOptions: {
+      tsconfig: true
+    },
+    meta: {
+      'typescript': {
+        "exports": "ts"
+      }
+    },
     map: map,
     packages: packages
   };
@@ -60,3 +84,10 @@
   System.config(config);
 
 })(this);
+
+
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Use of this source code is governed by an MIT-style license that
+ can be found in the LICENSE file at http://angular.io/license
+ */
