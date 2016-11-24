0fca8ec0c0be5f05780e2c243574f870a313741b
Paul Nadolny <psnadolny@gmail.com>
2016-11-23 21:29:02 -0600

    Angular 2 development, Bootstrap 4

--- cb2/src/app/app.component.ts
+++ cb2/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from "@angular/core";
 import {AppService} from "./app.service";
-import {Transaction} from "./app.model";
+import {Transaction, Entity} from "./app.model";
 import {BehaviorSubject} from "rxjs";
 import {asObservable} from "./asObservable";
 import {List} from "immutable";
+import * as moment from 'moment';
 
 
 @Component({
@@ -25,27 +26,17 @@ export class AppComponent implements OnInit {
 
     this.appService.get().subscribe(result => {
       let transactions = (result.json() as Transaction[]);
-
       let t = List<Transaction>(transactions);
       this._transactions.next(t);
     });
 
 
     this._transactions.subscribe(transactions => {
-
-      transactions.reverse();
-
+      let balance = 0.00;
       for (let t of transactions.toArray()) {
-
-          let tt = t as Transaction;
-
-          console.log(tt.amount);
-
-      }
-
-
-
-
+          balance = Number.parseFloat(t.amount) + balance;
+          t.balance = balance;
+     }
     })
   }
 
@@ -55,26 +46,52 @@ export class AppComponent implements OnInit {
 
   delete(transaction: Transaction) {
     this.appService.delete(transaction).subscribe(res => {
-
-
       let transactions: List<Transaction> = this._transactions.getValue();
       let index = transactions.findIndex((r) => r.name == transaction.name);
       this._transactions.next(transactions.delete(index));
-
-
     })
-
   }
+
+  copy(transaction: Transaction, nextMonth: boolean) {
+    let copy = new Transaction();
+    copy.name="";
+    copy.type = transaction.type;
+    copy.date = transaction.date;
+
+    if (nextMonth) {
+
+      copy.date = moment.utc(copy.date,"'YYYY-MM-DD HH:mm:ss.SSS-05:00')").add(1,'month').toISOString();
+
+    }
+
+    copy.amount = transaction.amount;
+    copy.memo = transaction.memo;
+    copy.payee = transaction.payee;
+    this.appService.save(copy).subscribe(response => {
+      let entity = (response.json() as Entity);
+      copy.name = "" + entity.key.id;
+      this._transactions.next(this._transactions.getValue().push(copy));
+      console.log(response);
+    })
+  }
+
   edit(transaction: Transaction) {
     this.transaction = transaction;
   }
 
   save(transaction: Transaction) {
-
     this.appService.save(transaction).subscribe(response => {
-
-
+      let entity = (response.json() as Entity);
+      if (transaction.name ==null) {
+        transaction.name = ""+entity.key.id;
+        this._transactions.next(this._transactions.getValue().push(transaction));
+     } else {
+        this._transactions.next(this._transactions.getValue())
+      }
+      this.transaction = null;
       console.log(response);
+    },() => {
+
 
     })
   }
--- cb2/src/app/app.model.ts
+++ cb2/src/app/app.model.ts
@@ -1,11 +1,21 @@
-export interface Transaction {
+export class Transaction {
   name: string;
   payee: string;
-  date: Date;
-  amount: number;
+  date: string;
+  amount: string;
   type: string;
   balance: number;
   memo: string;
 
 }
 
+export class Entity {
+  key: Key;
+
+
+}
+
+export class Key {
+  id:number;
+
+}
